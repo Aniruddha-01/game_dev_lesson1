80190ce438bcde279bdd7ab8aefadb869c8f2411
Aniruddha-01 <anilele2707@gmail.com>
2024-12-15 11:56:24 +0100

    Add files via upload

---
+++ lesson4.py
@@ -0,0 +1,23 @@
+#1d lists
+
+fruits = ["apple", "banana", "pineapple"]
+
+#Check length of list - len function
+print(len(fruits))
+#Adding values - append functions
+fruits.append("kiwi")
+
+#Adding values - insert function
+fruits.insert(1, "strawberry")
+
+#Removing values - remove function
+fruits.remove("apple")
+
+#2d lists
+matrix = [[1,2,3] , [4,5,6] , [7,8,9]]
+
+#number of rows - len function
+print(len(matrix))
+
+#number of columns
+print(len(matrix[1]))

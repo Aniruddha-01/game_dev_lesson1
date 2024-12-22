b7800711e042a3889cacb635eee48c5389e8e5f3
Aniruddha-01 <anilele2707@gmail.com>
2024-12-22 11:06:28 +0100

    Add files via upload

--- Lesson3.py
+++ Lesson3.py
@@ -2,32 +2,32 @@ import pgzrun
 from random import randint
 
 TITLE = "Good Shot"
-WIDTH = 500
-HEIGHT = 500
+WIDTH = 900
+HEIGHT = 900
 
 message = ""
 
-alien = Actor('alien')
-alien.pos = 50,50
+ghost = Actor('ghost')
+ghost.pos = 50,50
 
 def draw():
     screen.clear()
-    alien.draw()
+    ghost.draw()
     screen.draw.text(message , (10,10) , fontsize = 30)
 
-def place_alien():
-    alien.x = randint(50,WIDTH-50)
-    alien.y = randint(50,HEIGHT-50)
+def place_ghost():
+    ghost.x = randint(50,WIDTH-50)
+    ghost.y = randint(50,HEIGHT-50)
 
 def on_mouse_down(pos):
     global message
-    if alien.collidepoint(pos):
+    if ghost.collidepoint(pos):
         message = "Good shot"
-        place_alien()
+        place_ghost()
     else : 
         message = "Missed it"
 
-place_alien()
+place_ghost()
 
 
 

af46b3a050d19f55d95c293153d08ac740f598cc
Aniruddha-01 <anilele2707@gmail.com>
2024-12-29 11:33:06 +0100

    Add files via upload

--- l4_HW.py
+++ l4_HW.py
@@ -1,11 +1,31 @@
-fruits = ['apple', 'banana', 'orange']
 
-fruits.append('mango')
-print(fruits)
-fruits.remove('apple')
 
-print(fruits)
-print(fruits[1])
+list = ['Ani', 'Rishaan', 'Aadil']
+print(list)
+while(True)
+    print('This is the current list', list)
+    operation = int(input('Chose one of the following : 1. Add a name to the list, 2.Change a name in the list, 3.Delete a name from the list, 4.View all names from the list '))
 
-fruits.reverse()
-print(fruits)
+    if operation == 1:
+        element = input('What do you want to add ')
+        list.append(element)
+        print('This is the current list', list)
+
+    elif operation == 2:
+        index = int(input('Enter the index of the name you want to change '))
+        new_name = input('Enter the new name ')
+        list[index]=new_name
+        print('This is the updated list', list)
+
+    elif operation == 3:
+        delete_name = ('Enter the name you want to delete ')
+        list.remove(delete_name)
+
+    elif operation == 4:
+        for i in list:
+            print(i)
+
+    elif operation == 5:
+        break
+    else:
+        print('Please choose a valid input')

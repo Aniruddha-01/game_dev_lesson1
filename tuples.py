320e578ca1825e6a43d78fe2a3fcfdc791601d90
Aniruddha-01 <anilele2707@gmail.com>
2025-01-12 11:47:51 +0100

    Add files via upload

---
+++ tuples.py
@@ -0,0 +1,33 @@
+# # Tuple - An immutable collection of data
+# flowers = ('lily','sunflower','rose','lotus')
+
+# # print
+# print(flowers)
+
+# # Accessing the elements
+# print(flowers[1])
+
+# # Slicing
+# flowers1 = flowers[0:2]
+# print(flowers1)
+
+# print(flowers[:3])
+# print(flowers[1:])
+
+# Concatonation : Two or more tuples joined to created a new one
+tuple1 = ('rose','lily','sunflower','lotus')
+# tuple2 = ('ani, kim, am')
+# tuple3 = tuple2 + tuple1
+# print(tuple3)
+
+# Repitition
+# print(tuple1*2)
+
+# Length 
+# print(len(tuple1))
+
+# packng
+tuple_pack = (2, 'apple', False)
+
+# unpacking
+age , fruit , answer = (tuple_pack)

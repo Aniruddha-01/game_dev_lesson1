c89527eafab1ae83efe09cdb8b53641714643d94
Aniruddha-01 <anilele2707@gmail.com>
2025-01-19 11:15:26 +0100

    Update tuples_HW.py

--- tuples_HW.py
+++ tuples_HW.py
@@ -1,14 +1,13 @@
+for i in range(1,6):
+    grp_name = input('Enter the name of your group ')
+    grp_size = int(input('Enter the size of your group '))
+    comp_date = input('Enter the competition date ')
+    venue = input('Enter the venue of the competition ')
+    medal_type = input('Enter the type of medal recieved ')
 
-for i in range(5):
-    grp_name = (input('Enter the name of your group '))
-    grp_size = (input('Enter the size of your group '))
-    comp_date = (input('Enter the competition date '))
-    venue = (input('Enter the venue of the competition '))
-    medal_type = (input('Enter the type of medal recieved '))
+    group_info = (grp_name,grp_size,comp_date,venue,medal_type)
 
-    group_info = grp_name + grp_size + comp_date + venue + medal_type
-
-    print(group_info)
+    print("group number",i, group_info)
 
 
     

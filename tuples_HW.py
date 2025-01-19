b8afb1c2ee48969fc18e90c8e01986860f963ee1
Aniruddha-01 <anilele2707@gmail.com>
2025-01-19 11:00:36 +0100

    Add files via upload

---
+++ tuples_HW.py
@@ -0,0 +1,14 @@
+
+for i in range(5):
+    grp_name = (input('Enter the name of your group '))
+    grp_size = (input('Enter the size of your group '))
+    comp_date = (input('Enter the competition date '))
+    venue = (input('Enter the venue of the competition '))
+    medal_type = (input('Enter the type of medal recieved '))
+
+    group_info = grp_name + grp_size + comp_date + venue + medal_type
+
+    print(group_info)
+
+
+    

fbfaf188864ac499ae3b9cee3f978f8d3d60252d
Aniruddha-01 <anilele2707@gmail.com>
2025-02-09 11:18:58 +0100

    Add files via upload

--- connecting_satellites.py
+++ connecting_satellites.py
@@ -4,7 +4,7 @@ from time import time
 
 # game screen
 WIDTH = 800
-HEIGHT = 400
+HEIGHT = 600
  
 satellites = []
 lines = []
@@ -16,11 +16,11 @@ start_time = 0
 end_time = 0
 total_time = 0
 
-number_of_satellites = 8
+number_of_satellite = 8
 
-def create_satellite():
+def create_satellites():
     global start_time
-    for count in range(0, number_of_satellites):
+    for count in range(0, number_of_satellite):
         satellite = Actor('satellite')
         satellite.pos = randint(40,WIDTH-50) , randint(40,HEIGHT-50)
         satellites.append(satellite)
@@ -32,13 +32,13 @@ def draw():
     # drawing the numbers below the satellite
     number = 1
     for satellite in satellites:
-        screen.draw.text(str(number) , (satellite.pos[0] , satellite.pos[1] +20))
+        screen.draw.text(str(number) , (satellite.pos[0] , satellite.pos[1]+20))
         satellite.draw()
         number = number + 1 
     for line in lines:
         screen.draw.line(line[0], line[1], (255,255,255))
     
-    if next_satellite < number_of_satellites:
+    if next_satellite < number_of_satellite:
         total_time = time() - start_time
         screen.draw.text(str(round(total_time,1)), (10,10) , fontsize = 30)
     else :
@@ -50,7 +50,7 @@ def update():
 
 def on_mouse_down(pos):
     global next_satellite , lines
-    if next_satellite < number_of_satellites:
+    if next_satellite < number_of_satellite:
         if satellites[next_satellite].collidepoint(pos):
             if next_satellite:
                 lines.append( (satellites[next_satellite-1].pos, satellites[next_satellite].pos) )
@@ -59,7 +59,7 @@ def on_mouse_down(pos):
             lines=[]
             next_satellite = 0
 
-create_satellite()
+create_satellites()
 
 
 pgzrun.go()

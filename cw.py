bb307430d2c7b8ef29dc8167ce2aab0561dbd683
Aniruddha-01 <anilele2707@gmail.com>
2025-02-23 11:53:57 +0100

    Add files via upload

--- cw.py
+++ cw.py
@@ -14,10 +14,11 @@ START_SPEED = 10
 ITEMS = ["bag","battery","bottle","chips"]
 
 game_over = False
-game_complete = True
+game_complete = False
 current_level = 1
 items = []
 animations = []
+
 
 def draw():
     global items,current_level,game_over,game_complete
@@ -32,10 +33,56 @@ def draw():
             item.draw()
 
 
+def get_options_to_create(number_of_extra_items):
+    items_to_create = ["paper"]
+    for i in range (0,number_of_extra_items):
+        random_option = random.choice(ITEMS)
+        items_to_create.appemd(random_option)
+    return items_to_create
+
+
+def create_items(items_to_create):
+    new_items=[]
+    for options in items_to_create:
+        item = Actors(option + "img")
+        new_items.append(item)
+    return new_items
+
+
+def layout_items(items_to_layout):
+    number_of_gaps = len(items_to_layout) + 1
+    gap_size = WIDTH/number_of_gaps
+    random.shuffle(items_to_layout)
+    for index, item in enumerate(items_to_layout):
+        new_x_pos = (index+1)*gap_size
+        item.x = new_x_pos
+
+def animate_items(items_to_animate):
+    global animations
+    for item in items_to_animate:
+        duration = START_SPEED - current_level
+        item.anchor = ("center","bottom")
+        animation = animate(item ,duration=duration ,on_finished = handle_game_over() y = HEIGHT)
+        animations.append(animation)
+
+
+def update():
+
+
+def make_items(number_of_extra_items):
 
 
 
+def handle_game_over():
 
+
+def handle_game_complete():
+
+
+def on_mouse_down():
+
+
+def stop_animations():
 
 
 

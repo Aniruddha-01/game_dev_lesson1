03b57375d430333aaf4f5e425e1671bc63a0527e
Aniruddha-01 <anilele2707@gmail.com>
2025-03-23 10:39:08 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -4,14 +4,14 @@ TITLE = 'Quiz_Master'
 WIDTH = 870
 HEIGHT = 650
 
-marquee_box = Rect(0,0 , 880,80)
+marquee_box = Rect(0,0 , 870,80)
 answer_box_1 = Rect(0,0 , 300,150)
 answer_box_2 = Rect(0,0 , 300,150)
 answer_box_3 = Rect(0,0 , 300,150)
 answer_box_4 = Rect(0,0 , 300,150)
-question_box = Rect(0,0 , 750,70)
-timer_box = Rect(0,0 , 70,70)
-skip_box = Rect(0,0 , 70,300)
+question_box = Rect(0,0 , 630,70)
+timer_box = Rect(0,0 , 120,70)
+skip_box = Rect(0,0 , 120,300)
 
 marquee_box.move_ip(0,0)
 question_box.move_ip(20,100)
@@ -19,14 +19,22 @@ answer_box_1.move_ip(20,190)
 answer_box_2.move_ip(350,190)
 answer_box_3.move_ip(20,360)
 answer_box_4.move_ip(350,360)
-timer_box.move_ip(800,100)
-skip_box.move_ip(800,200)
+timer_box.move_ip(700,100)
+skip_box.move_ip(700,190)
 
 def draw():
     screen.clear()
     screen.fill(color = "black")
     screen.draw.filled_rect(marquee_box,"green")
+    screen.draw.filled_rect(question_box,"red")
+    screen.draw.filled_rect(answer_box_1,"orange")
+    screen.draw.filled_rect(answer_box_2,"orange")
+    screen.draw.filled_rect(answer_box_3,"orange")
+    screen.draw.filled_rect(answer_box_4,"orange")
+    screen.draw.filled_rect(timer_box,"blue")
+    screen.draw.filled_rect(skip_box,"blue")
 
+    
 
 
 

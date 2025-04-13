c038dc2d9b13086b733f32f3a1fc4275a284f943
Aniruddha-01 <anilele2707@gmail.com>
2025-04-13 11:46:15 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pgzrun
-
+import time
 TITLE = 'Quiz_Master'
 WIDTH = 870
 HEIGHT = 650
@@ -25,10 +25,12 @@ skip_box.move_ip(700,190)
 
 score = 0
 time_left = 10
-question_file_name = "questions.txt"
+question_file_name = r"D:\OneDrive\Desktop\Python_gamedev\Quiz_master\questions.txt"
 questions = []
 question_count = 0
-question_index = 1
+question_index = 0
+answer_boxes = [answer_box_1,answer_box_2,answer_box_3,answer_box_4]
+
 
 def draw():
     screen.clear()
@@ -45,7 +47,12 @@ def draw():
     marquee_text = "Welcome to QuizMaster..."
     marquee_text = marquee_text + f"Q:{question_index} of {question_count}"
     screen.draw.textbox(marquee_text,marquee_box,color="black")
-
+    screen.draw.textbox(question[0].strip(),question_box,color="black" , angle = 0)
+    index = 1
+    for answer_box in answer_boxes:
+        screen.draw.textbox(question[index].strip(),answer_box,color="blue")
+        index +=1
+    screen.draw.textbox(str(time_left),timer_box,color="black")
 def update():
     move_marquee()
 
@@ -65,8 +72,58 @@ def read_q_file():
    
 
 
+def read_next_question():
+    global question_index
+    question_index +=1
+    return questions.pop(0).split(",")
+ 
+def game_over():
+    global question,time_left,score 
+    message = f"Game Over! \n You got {score} questions correct"
+    question=[message, "-","-","-","-",5]
+    time_left=0
+
+def correct_ans():
+    global score,question,questions,time_left
+    if questions:
+        question=read_next_question()
+        score += 1 
+        time_left = 10
+    else:
+        game_over()
+   
+
+def timer():
+    global time_left
+    if time_left:
+        time_left -=1 
+    else:
+        game_over()
 
 
+def skip_question():
+    global questions,question,time_left
+    if questions:
+        question = read_next_question()
+        time_left = 10
+    else:
+        game_over()
+
+
+
+
+def on_mouse_down(pos):
+    index=1
+    for box in answer_boxes:
+        if box.collidepoint(pos):
+            if index is int(question[5]):
+                correct_ans()
+            else:
+                game_over()
+        index +=1
+    if skip_box.collidepoint(pos):
+        skip_question()
+        
 
 
 
@@ -76,4 +133,6 @@ def read_q_file():
 
 
 read_q_file()
+question=read_next_question()
+clock.schedule_interval(timer,1)
 pgzrun.go()

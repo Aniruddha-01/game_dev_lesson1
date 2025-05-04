c2cb75db5119c90b5fddbac395818df987958752
Aniruddha-01 <anilele2707@gmail.com>
2025-05-04 11:19:21 +0200

    Add files via upload

--- comp_data.py
+++ comp_data.py
@@ -1,11 +1,17 @@
 
 group_num = 1
-for i in range(5):
+comp_data = []
+for i in range(2):
     print('Group',group_num)
     group_name = input('Enter the name of your group ')
     group_size = input('Enter the size of your group ')
     comp_date = input('Enter the date of the competition in DD-MM-YYYY ')
     medal_type = input('Enter the type of medal recieved ')
-    group= (group_name,group_size,comp_date,medal_type) 
-    print(group,group_num) 
+    comp_data.append((group_name,group_size,comp_date,medal_type)) 
     group_num += 1
+
+
+for group in comp_data:
+    print("****************************")
+    for element in group:
+        print(element)

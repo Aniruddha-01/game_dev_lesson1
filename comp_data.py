8538a4dd8a9e8a38f3688cecf180262417c8cd8e
Aniruddha-01 <anilele2707@gmail.com>
2025-05-04 10:53:44 +0200

    Add files via upload

---
+++ comp_data.py
@@ -0,0 +1,11 @@
+
+group_num = 1
+for i in range(5):
+    print('Group',group_num)
+    group_name = input('Enter the name of your group ')
+    group_size = input('Enter the size of your group ')
+    comp_date = input('Enter the date of the competition in DD-MM-YYYY ')
+    medal_type = input('Enter the type of medal recieved ')
+    group= (group_name,group_size,comp_date,medal_type) 
+    print(group,group_num) 
+    group_num += 1
